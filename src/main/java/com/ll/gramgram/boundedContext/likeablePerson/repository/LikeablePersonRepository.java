e23bef190138d685c89147fdce116e927ddcbeeb
LeeYulhee <reasonoflife39@gmail.com>
2023-04-14 19:00:02 +0900

    BaseEntity 추가

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/repository/LikeablePersonRepository.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/repository/LikeablePersonRepository.java
@@ -9,4 +9,9 @@ import java.util.Optional;
 public interface LikeablePersonRepository extends JpaRepository<LikeablePerson, Long> {
     List<LikeablePerson> findByFromInstaMemberId(Long fromInstaMemberId);
     List<LikeablePerson> findByToInstaMemberUsername(String toInstaMemberUsername);
+
+    List<LikeablePerson> findByToInstaMember_username(String username);
+    // JPA 함수에서 언더바를 사용하면 관련 엔티티의 필드까지 검색 조건으로 활용할 수 있음
+
+    LikeablePerson findByFromInstaMemberIdAndToInstaMember_username(long fromInstaMemberId, String username);
 }
